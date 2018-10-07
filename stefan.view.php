0270c76f3dd049d0415f5501f532aad3f1af1851
Samanthavermeulen <samantha.vermeulen@hotmail.com>
2018-10-07 16:02:59 +0200

    added some bootstrap

--- stefan.view.php
+++ stefan.view.php
@@ -30,18 +30,10 @@
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Nieuws</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown01">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
+                <a class="nav-link disabled" href="#">Media</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
